e8282bfb6ee9b64bf9b65b45e43b2684ae190fab
zanderson1998 <zanderson1998@yahoo.com>
2021-02-11 17:19:12 -0700

    add Post routes

--- models/User.js
+++ models/User.js
@@ -26,7 +26,8 @@ User.init(
         //define a username column
         username: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true,
         },
         //define an email column
         email: {
