e0cb1f47845b6eefc1e84825c0d6500c4fa01d3b
zanderson1998 <zanderson1998@yahoo.com>
2021-02-18 16:46:19 -0700

    add comment posting ability and hide login/logout respectively

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { Comment,User,Post } = require('../../models');
+const { Comment, User, Post } = require('../../models');
 
 router.get('/', (req, res) => {
     Comment.findAll({
-        attributes: ['id','comment_text','user_id','post_id','created_at'],
+        attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
         include: [
             {
                 model: User,
@@ -15,24 +15,28 @@ router.get('/', (req, res) => {
             }
         ]
     })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    // check the session
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // use the id from the session
+            user_id: req.session.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 router.delete('/:id', (req, res) => {
@@ -41,17 +45,17 @@ router.delete('/:id', (req, res) => {
             id: req.params.id
         }
     })
-    .then(dbCommentData => {
-        if (!dbCommentData) {
-            res.status(404).json({ message: 'No comment found with this id' })
-            return;
-        }
-        res.json(dbCommentData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id' })
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,13 +87,15 @@ router.post('/', (req, res) => {
 //upvote a post
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
+    if (req.session) {
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+          console.log(err);
+          res.status(500).json(err);
         });
-});
+    }
+  });
 
 //update a user
 router.put('/:id', (req, res) => {
